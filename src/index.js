1004bea2d625674d826a9ef27218a5093d8c32c2
danhamraj <danhamraj@gmail.com>
2019-12-10 23:16:44 -0500

    Added dependancies, imported createStore, provider and created ruducer folder and file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux"; //2. import createStore
+import { Provider } from "react-redux"; //4. imp. provider
+//import vehReducer from './reducers/vehReducer; 3. imp reducer
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
+//1. create store and pass in reducer
+const store = createStore(vehReducer);
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, //5. wrap App in Provider pass in store
+  rootElement
+);
